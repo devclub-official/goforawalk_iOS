e197a03d12d2c99b8896b479b2d724f53048eec2
gaeng2y <gaeng2y@gmail.com>
2025-04-14 20:51:18 +0900

    ➕ GW-68 카카오 SDK 의존성 추가

--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -5,6 +5,7 @@ public extension TargetDependency {
 }
 
 public extension TargetDependency.SPM {
+    static let kakaoSDK = TargetDependency.external(name: "KakaoOpenSDK")
     static let tca = TargetDependency.external(name: "swift-composable-architecture")
 }
 
